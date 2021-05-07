36ac13657b0b304cdb37772c9f216706e7064209
Marco Antonio Gomes Bernardino <marcoagbernardino@gmail.com>
2021-05-06 22:00:08 -0300

    Group by search

--- backend/src/main/java/com/devsuperior/dsvendas/dto/SellerDTO.java
+++ backend/src/main/java/com/devsuperior/dsvendas/dto/SellerDTO.java
@@ -7,6 +7,7 @@ import com.devsuperior.dsvendas.entities.Seller;
 public class SellerDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	
 	private Long id;
 	private String name;
 	
